b808616f6847bdfe40b6eecfe5c2af0e6b7d168e
Mark Miller <725mrm@gmail.com>
2018-10-14 12:41:26 -0700

    a game end state and a little bit of music!

---
+++ Assets/InstructionsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionsManager : MonoBehaviour {
+
+	public Text mainText;
+	public GameObject[] buttons;
+	public int step = 0;
+	public string[] instructionSteps;
+	public ThrowEnergy throwing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StepForwardInDialogue() {
+		buttons [step].SetActive (false);
+		step++;
+		if (step < buttons.Length && buttons [step] && step < instructionSteps.Length) {
+			mainText.text = instructionSteps [step];
+			buttons [step].SetActive (true);
+		} else {
+			gameObject.SetActive (false);
+			throwing.isSending = true;
+		}
+	}
+}
--- Assets/ThrowEnergy.cs
+++ Assets/ThrowEnergy.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThrowEnergy : MonoBehaviour {
 
 	public GameObject target;
 	public GameObject projectile;
 	float speed = 1.0f;
-
+	public bool isSending = false;
+	public GameObject instructionsManager;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +18,15 @@ public class ThrowEnergy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.frameCount % 200 == 0) {
+		if (Time.frameCount % 200 == 0 && isSending) {
+			if (GetComponent<HealthManager> ().health < 0.1f) {
+				// game over!
+				instructionsManager.SetActive(true);
+				Text mainText = instructionsManager.GetComponentInChildren<Text> ();
+				mainText.text = "I don't think I can carry on...";
+				isSending = false;
+
+			}
 			GameObject newProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
 
 			Vector3 offset = target.transform.position - this.transform.position;
@@ -32,6 +42,12 @@ public class ThrowEnergy : MonoBehaviour {
 		if (other.CompareTag ("ScreenPhysical") && other.GetComponent<ReboundFlag>().hasRebounded) {
 			GetComponent<HealthManager>().health += 0.1f * other.GetComponent<Rigidbody> ().velocity.magnitude;
 			Destroy (other);
+			if (GetComponent<HealthManager> ().health > 1.0f) {
+				instructionsManager.SetActive (true);
+				Text mainText = instructionsManager.GetComponentInChildren<Text> ();
+				mainText.text = "Thank you! Now I can continue on my journey back home!";
+				isSending = false;
+			}
 		}
 	}
 }
